0f7c1cd19cb49eed8d4a6885097b9aa040d30229
Mateo <mateo.juginovic@gmail.com>
2021-04-09 19:07:46 +0200

    Dodan modal unutar html-a za unos podataka eventa. Formirane osnove potrebnog js koda

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
-<div class="row text-center" style="margin-top: 40px; margin-bottom: 30px;">
-            <div class="col"><h2>Najnoviji događaji</h2></div>
-        </div>
+function Event(naziv, opis, datum, vrijeme, grad, adresa, vrsta){
+    this.naziv = naziv;
+    this.opis = opis;
+    this.datum = datum;
+    this.vrijeme = vrijeme;
+    this.grad = grad;
+    this.adresa = adresa;
+    this.vrsta = vrsta;
+    this.ispis = function(){
+        console.log(this.naziv + " " + this.opis + " " + this.datum + " " + this.vrijeme + " " + this.grad + " " + this.adresa + " " + this.vrsta);
+    }
+}
 
-        <div class="row text-center" style="margin-bottom: 40px;">
-            <div class="col">
-                <button class="btn-outline-info btn" type="button">Kultura</button>
-                <button class="btn-outline-info btn" type="button">Koncerti</button>
-                <button class="btn-outline-info btn" type="button">Sport</button>
-                <button class="btn-outline-info btn" type="button">Protesti</button>
-                <button class="btn-outline-info btn" type="button">Ostalo</button>
-            </div>
-        </div>
-
-        <div class="row" style="margin-bottom: 10px;">
-            <div class="col">
-                <button class="btn-outline-dark btn" type="button">Prošlo</button>
-                <button class="btn-outline-dark btn" type="button">Danas</button>
-                <button class="btn-outline-dark btn" type="button">Sutra</button>
-                <button class="btn-outline-dark btn" type="button">Ovaj tjedan</button>
-                <button class="btn-outline-dark btn" type="button">Ostalo</button>
-            </div>
-        </div>
-
-        <div class="row sadrzaj" style="margin-bottom: 20px;">
-
-            <div class="col-lg-3 col-md-4 col-sm-12" style="margin-bottom: 20px;">
-                <div class="card" style="width: 19rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Jelena Rozga, koncert</h5>
-                        <p class="card-text">OPIS: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">28.6.2020. - 29.6.2020.</li>
-                        <li class="list-group-item">Početak: 15:30</li>
-                        <li class="list-group-item">Ulica kralja Zvonimira, 41</li>
-                    </ul>
-                </div>
-            </div>
-            
-            
-            
-
-        </div>
+function unos(){
+    let naziv = document.getElementById("nazivUnos").value;
+    let opis = document.getElementById("opisUnos");
+    let datum = document.getElementById("datumUnos");
+    let vrijeme = document.getElementById("vrijemeUnos");
+    let grad = document.getElementById("gradUnos");
+    let adresa = document.getElementById("adresaUnos");
+    let vrsta = document.getElementById("vrstaUnos");
+    
+    event1 = new Event(naziv, opis, datum, vrijeme, grad, adresa, vrsta);
+    
+    event1.ispis();
+}
